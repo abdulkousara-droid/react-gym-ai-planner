e603ad814a1e3c56f37adce0650d0135c0e8e833
abdulkousara-droid <abdulkader.kousara@gmail.com>
2026-04-23 01:31:59 +0200

    fixing neonauth clint config

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,4 @@
 import { createAuthClient } from '@neondatabase/neon-js/auth';
 export const authClient = createAuthClient(
     import.meta.env.VITE_NEON_AUTH_URL,
-    import.meta.env.VITE_API_URL
 )
