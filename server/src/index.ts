c9a7f62b4fed445d20b4149ea254fa74d3d4b895
abdulkousara-droid <abdulkader.kousara@gmail.com>
2026-04-23 01:16:20 +0200

    fixing cors error in backend server

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,7 +10,19 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors());
+
+const allowedOrigins = [
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "https://react-gym-ai-planner-seven.vercel.app/",
+    "https://ep-twilight-firefly-an37z0qp.neonauth.c-6.us-east-1.aws.neon.tech/neondb/auth",
+];
+
+app.use(cors({
+    origin: allowedOrigins,
+    credentials: true,
+}));
+//app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 
